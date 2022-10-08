158ceaa44704ab4236a7ba6724617db14479c1cf
Luis Dourado <lfaddourado@gmail.com>
2022-10-08 15:29:09 -0300

    add toggle funcionality to  small sidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,6 +1,34 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleSidebar } from '../features/user/userSlice';
 
 const SmallSidebar = () => {
-  return <Wrapper>s</Wrapper>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <Logo />
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <div className="nav-links">a</div>
+          </header>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default SmallSidebar;
