9de4b3c6efea8e585a00be8b0adc1a188b927f02
Luis Dourado <lfaddourado@gmail.com>
2022-10-08 15:57:04 -0300

    add big sidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,6 +1,24 @@
 import Wrapper from '../assets/wrappers/BigSidebar';
-
+import NavLinks from './NavLinks';
+import { useSelector } from 'react-redux';
+import Logo from './Logo';
 const BigSidebar = () => {
-  return <Wrapper>sa</Wrapper>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default BigSidebar;
