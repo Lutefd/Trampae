6b900be037a963a032cc11f0e6b723366c724b23
Luis Dourado <lfaddourado@gmail.com>
2022-10-08 17:29:21 -0300

    add jobs page structure

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,4 +1,38 @@
+import Wrapper from '../assets/wrappers/JobsContainer';
+import Job from './Job';
+import Loading from './Loading';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 const JobsContainer = () => {
-  return <div>b</div>;
+  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  if (isLoading) {
+    return (
+      <Wrapper>
+        <Loading />
+      </Wrapper>
+    );
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>Nenhum trampo para mostrar...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <h5>Informações dos Trampos</h5>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 export default JobsContainer;
