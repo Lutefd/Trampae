c53845d3ad2bd1bc59c8a18f3ec878f6b563695a
Luis Dourado <lfaddourado@gmail.com>
2022-10-08 10:46:31 -0300

    add react icons lib

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,10 @@
+import Wrapper from '../assets/wrappers/Navbar';
+import { FaHome } from 'react-icons/fa';
 const Navbar = () => {
-  return <div>asd</div>;
+  return (
+    <Wrapper>
+      <FaHome />
+    </Wrapper>
+  );
 };
 export default Navbar;
