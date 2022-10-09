4d03f13d1b085303c43ba58182991e0ce81f926c
Luis Dourado <lfaddourado@gmail.com>
2022-10-09 12:20:56 -0300

    add search container structure

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,4 +1,65 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { FormRow, FormRowSelect } from '.';
+import Wrapper from '../assets/wrappers/SearchContainer';
+
 const SearchContainer = () => {
-  return <div>a</div>;
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+    useSelector((store) => store.allJobs);
+  const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
+  const dispatch = useDispatch();
+  const handleSearch = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Wrapper>
+      <form className="form">
+        <h4>Pesquisar teus Trampos</h4>
+        <div className="form-center">
+          {/* search position */}
+
+          <FormRow
+            type="text"
+            labelText="Nome"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          />
+          {/* search by status */}
+          <FormRowSelect
+            labelText="status"
+            name="searchStatus"
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          />
+          {/* search by type */}
+          <FormRowSelect
+            labelText="Tipo de Trampo"
+            name="searchType"
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          />
+          {/* sort */}
+          <FormRowSelect
+            name="sort"
+            labelText="Ordenar"
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button
+            className="btn btn-block btn-danger"
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            Limpar Filtros
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
+
 export default SearchContainer;
