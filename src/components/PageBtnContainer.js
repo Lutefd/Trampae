a81152be7084a55af67b678bfb24b1e3b0eb9bbe
Luis Dourado <lfaddourado@gmail.com>
2022-10-09 12:34:36 -0300

    add page button component

--- src/components/PageBtnContainer.js
+++ src/components/PageBtnContainer.js
@@ -1,4 +1,41 @@
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
+import Wrapper from '../assets/wrappers/PageBtnContainer';
+import { useSelector, useDispatch } from 'react-redux';
 const PageBtnContainer = () => {
-  return <div>a</div>;
+  const { numOfPages, page } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+  const nextPage = () => {};
+  const prevPage = () => {};
+  return (
+    <Wrapper>
+      <button className="prev-btn" onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        Anterior
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              type="button"
+              className={pageNumber === page ? 'pageBtn active' : 'pageBtn'}
+              key={pageNumber}
+              onClick={() => console.log('change page')}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button className="next-btn" onClick={nextPage}>
+        Próxima
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
 };
+
 export default PageBtnContainer;
