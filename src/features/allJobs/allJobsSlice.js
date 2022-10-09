45e9fe1dc0bd6ad56f0953c944ca91ac5f8ef8f1
Luis Dourado <lfaddourado@gmail.com>
2022-10-09 12:20:49 -0300

    Add reducers; change sort options

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -7,7 +7,7 @@ const initialFiltersState = {
   searchStatus: 'all',
   searchType: 'all',
   sort: 'latest',
-  sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
+  sortOptions: ['Mais Recente', 'Mais Antigo', 'A-Z', 'Z-A'],
 };
 
 const initialState = {
@@ -65,6 +65,13 @@ const allJobsSlice = createSlice({
     hideLoading: (state) => {
       state.isLoading = false;
     },
+    handleChange: (state, { payload: { name, value } }) => {
+      // state.page = 1;
+      state[name] = value;
+    },
+    clearFilters: (state) => {
+      return { ...state, ...initialFiltersState };
+    },
   },
   extraReducers: {
     [getAllJobs.pending]: (state) => {
@@ -94,4 +101,5 @@ const allJobsSlice = createSlice({
 });
 
 export default allJobsSlice.reducer;
-export const { showLoading, hideLoading } = allJobsSlice.actions;
+export const { showLoading, hideLoading, handleChange, clearFilters } =
+  allJobsSlice.actions;
