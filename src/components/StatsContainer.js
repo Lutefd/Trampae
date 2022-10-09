6b63a019f184bb9cffd0c7b655c55ef68e3ac389
Luis Dourado <lfaddourado@gmail.com>
2022-10-09 11:50:34 -0300

    add stats container and items

--- src/components/StatsContainer.js
+++ src/components/StatsContainer.js
@@ -1,4 +1,40 @@
+import { useSelector } from 'react-redux';
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from 'react-icons/fa';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import StatItem from './StatItem';
 const StatsContainer = () => {
-  return <div>a</div>;
+  const { stats } = useSelector((store) => store.allJobs);
+  const defaultStats = [
+    {
+      title: 'Aplicações pendentes',
+      count: stats.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: '#e9b949',
+      bcg: '#fcefc7',
+    },
+    {
+      title: 'Entrevistas marcadas',
+      count: stats.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: '#647acb',
+      bcg: '#e0e8f9',
+    },
+    {
+      title: 'Trampos rejeitados',
+      count: stats.declined || 0,
+      icon: <FaBug />,
+      color: '#d66a6a',
+      bcg: '#ffeeee',
+    },
+  ];
+
+  return (
+    <Wrapper>
+      {defaultStats.map((item, index) => {
+        return <StatItem key={index} {...item} />;
+      })}
+    </Wrapper>
+  );
 };
+
 export default StatsContainer;
